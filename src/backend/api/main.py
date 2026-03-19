f0f4a625ac8b23a5ae1dd5e5077c08d545214c89
josias <josias.nanez@mi.unc.edu.ar>
2026-03-19 03:53:07 -0300

    fix: api create app

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
-from .routers import people_router
-from .routers import config_router
+from src.backend.app import create_app
+
+from .routers import people_router, config_router
 
 app = FastAPI(
     title="Renuevo API",
     version="1.0"
 )
+
+container = create_app()
 
 app.include_router(people_router.router)
 app.include_router(config_router.router)
