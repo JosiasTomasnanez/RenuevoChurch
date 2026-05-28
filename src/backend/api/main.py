87062663402719b1299b927895e7b3b30916de06
josias <josias.nanez@mi.unc.edu.ar>
2026-05-28 20:06:42 -0300

    web admitions

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,11 +1,20 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 from src.backend.app.main import create_app
 from .routers import people_router, config_router
 
 app = FastAPI(
     title="Renuevo API",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
 )
 
 container = create_app()
@@ -15,7 +24,6 @@ app.include_router(config_router.router)
 
 @app.get("/api/version")
 def get_software_version():
-    # Render va a mandar estos valores dinámicamente
     latest = os.environ.get("LATEST_VERSION", "1.0.0")
     download = os.environ.get("DOWNLOAD_URL", "")
 
@@ -26,7 +34,6 @@ def get_software_version():
 
 @app.get("/api/version-mobile")
 def get_mobile_software_version():
-    # Render va a mandar estos valores dinámicamente para la app móvil
     latest_mobile = os.environ.get("MOBILE_LATEST_VERSION", "1.0.0")
     download_mobile = os.environ.get("MOBILE_DOWNLOAD_URL", "")
     
